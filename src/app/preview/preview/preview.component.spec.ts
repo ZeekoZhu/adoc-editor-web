a9523e5c73b8a4a84240fce968d082d8ae874748
ZeekoZhu <vaezt@outlook.com>
2021-11-13 14:33:00 +0800

    test: add safe dom pipe to preview component test env

--- src/app/preview/preview/preview.component.spec.ts
+++ src/app/preview/preview/preview.component.spec.ts
@@ -1,25 +1,26 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PreviewComponent } from './preview.component';
+import { SafeDomPipe } from '@app/shared/pipes/safe-dom.pipe';
 
 describe('PreviewComponent', () => {
-  let component: PreviewComponent;
-  let fixture: ComponentFixture<PreviewComponent>;
+    let component: PreviewComponent;
+    let fixture: ComponentFixture<PreviewComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PreviewComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            declarations: [ PreviewComponent, SafeDomPipe ],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PreviewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PreviewComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
